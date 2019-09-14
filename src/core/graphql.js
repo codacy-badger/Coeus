0c0a201e38a5240e9d69cc88aae013c244fbff13
Steven J. Selcuk <stevenjselcuk@gmail.com>
2019-09-14 19:02:39 +0300

    h

--- src/core/graphql.js
+++ src/core/graphql.js
@@ -10,6 +10,15 @@ import { log, show } from '~/core/logger'
 
 import schema from '~/app/schema'
 
+const config = conf.get('IS_PROD')
+  ? {
+      port: conf.get('REDIS_PORT'),
+      host: conf.get('REDIS_URL'),
+      password: conf.get('REDIS_PASSWORD')
+    }
+  : conf.get('REDIS_URL')
+  
+const redisCache = new Redis(config)
 
 export const pubsub = new PubSub()
 
@@ -84,5 +93,9 @@ export default new ApolloServer({
     calculateHttpHeaders: false,
     // Cache everything for at least a minute since we only cache public responses
     defaultMaxAge: 6000
-  }
+  },
+  cache: new RedisCache({
+    redisCache,
+    prefix: 'apollo-cache:'
+  })
 })
