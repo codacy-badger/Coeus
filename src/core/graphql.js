6a867b66844ed67faf97db589ac6e6daae6a4376
Steven J. Selcuk <stevenjselcuk@gmail.com>
2019-09-14 19:11:38 +0300

    s

--- src/core/graphql.js
+++ src/core/graphql.js
@@ -17,7 +17,8 @@ const config = conf.get('IS_PROD')
       password: conf.get('REDIS_PASSWORD')
     }
   : conf.get('REDIS_URL')
-  
+  console.log(config)
+  console.log(conf.get('REDIS_URL'))
 const redisCache = new Redis(conf.get('REDIS_URL'))
 
 export const pubsub = new PubSub()
