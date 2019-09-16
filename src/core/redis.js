7db918271b3331bf8304ae5d789d2d73dce5c5bf
Steven J. Selcuk <stevenjselcuk@gmail.com>
2019-09-16 22:00:19 +0300

    redis

--- src/core/redis.js
+++ src/core/redis.js
@@ -1,5 +1,15 @@
 import Redis from 'ioredis'
 
-const redis = new Redis()
+
+const RedisConfig ={
+  port: 23339, // Redis port
+  host: "ec2-3-220-50-71.compute-1.amazonaws.com", // Redis host
+  family: 4, // 4 (IPv4) or 6 (IPv6)
+  password: "p1d3a9fee8e6af2fae8b18990ecffd65e2f8ddab903d4047d11a634422b15f799",
+	user: 'h',
+  db: 0
+}
+
+const redis = new Redis(RedisConfig)
 
 export default redis
