b3262cce1965b1f41ecde48ad7696b5217a87d99
Steven J. Selcuk <stevenjselcuk@gmail.com>
2019-09-16 23:22:48 +0300

    🐛 FIX: redis

--- src/core/redis.js
+++ src/core/redis.js
@@ -1,15 +1,18 @@
-import redis from 'redis'
+import Redis from 'ioredis'
 import conf from './config'
-let cache = ''
 
 
-if (process.env.REDIS_URI) {
-	const rtg   = require("url").parse(process.env.REDISTOGO_URL);
-  cache = redis.createClient(rtg.port, rtg.hostname);
+const config =
+  process.env.NODE_ENV === 'development'
+    ? {
+			  host: "ec2-3-215-116-159.compute-1.amazonaws.com",
+        port: 10059,
+        password: "p483f309a1223bd535c3be29ee1643076f430222eccd15b3e95e2d334a9efe519",
+      }
+    : undefined;
+		
+		
+		console.log(conf.get('REDIS_URI'))
+const redis = new Redis(conf.get('REDIS_URI'))
 
-redis.auth(rtg.auth.split(":")[1]);
-} else {
-    cache = redis.createClient();
-}
-
-export default cache
+export default redis
