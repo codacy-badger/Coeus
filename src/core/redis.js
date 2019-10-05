b4413c58513fa34331eb5a53f3146c6025ce1ce2
Steven J. Selcuk <stevenjselcuk@gmail.com>
2019-10-05 21:41:45 +0300

    Redis config

--- src/core/redis.js
+++ src/core/redis.js
@@ -1,7 +1,9 @@
 import Redis from 'ioredis'
 import conf from './config'
 
-const redis = new Redis(conf.get('REDIS_URI'))
+const REDIS_URI = conf.get('REDIS_URI') || 'redis://127.0.0.1:6379'
+
+const redis = new Redis(REDIS_URI)
 
 export default redis
 
