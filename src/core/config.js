2f6b923e80f98407d9d6553fdd42cc62f4cfca6f
Steven J. Selcuk <stevenjselcuk@gmail.com>
2020-02-27 20:40:19 +0300

    asdasdasd
    Signed-off-by: Steven J. Selcuk <stevenjselcuk@gmail.com>

--- src/core/config.js
+++ src/core/config.js
@@ -9,6 +9,7 @@ conf
     file: './config.json'
   })
 
+conf.set('PORT', process.env.PORT)
 conf.set('VERSION', version)
 conf.set('IS_PROD', process.env.NODE_ENV === 'production')
 conf.set('IS_DEV', process.env.NODE_ENV === 'development')
