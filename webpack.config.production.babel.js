c19ad36228666e5539b17aa6245f1a76f3675114
Steven J. Selcuk <stevenjselcuk@gmail.com>
2020-02-29 22:11:16 +0300

    asd
    Signed-off-by: Steven J. Selcuk <stevenjselcuk@gmail.com>

--- webpack.config.production.babel.js
+++ webpack.config.production.babel.js
@@ -61,7 +61,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
-        PORT: JSON.stringify('process.env.PORT')
+        PORT: JSON.stringify(process.env.PORT)
       }
     })
   ]
